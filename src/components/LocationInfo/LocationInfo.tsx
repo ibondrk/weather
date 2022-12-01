e238a7f85aa184e277456647de99cb128ac98e3d
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-12-01 05:13:37 +0200

    deleted comments

--- src/components/LocationInfo/LocationInfo.tsx
+++ src/components/LocationInfo/LocationInfo.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from './location.module.scss';
-// import { Loader } from '../Loader';
 
 import { useAppSelector } from '../../app/hooks';
 
@@ -11,9 +10,6 @@ export const LocationInfo: React.FC = () => {
     <div>
       <h2 className={style.loc}>
         Forecast in {weather?.location.name} {weather?.location.region}
-        {weather?.location.country}
-        {/* {weather && (
-        )} */}
         {weather?.location.country === 'Ukraine' && ' tryzub'}
       </h2>
     </div>
