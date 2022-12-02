3c4a734aeadb44483eabf32383eab2265dd486fd
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-12-02 18:55:59 +0200

    rebuilded weather table

--- src/components/WeatherInfo/WeatherInfo.tsx
+++ src/components/WeatherInfo/WeatherInfo.tsx
@@ -7,6 +7,10 @@ import { Hour } from '../../types/Forecast';
 
 import { useAppSelector } from '../../app/hooks';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper';
+import 'swiper/scss';
+
 export const WeatherInfo: React.FC = () => {
   const { selectedDay } = useAppSelector((state) => state.weather);
   const [weatherByHours, setWeatherByHours] = useState<Hour[]>([]);
@@ -50,157 +54,106 @@ export const WeatherInfo: React.FC = () => {
   }, [selectedDay]);
 
   return (
-    <div
-      className={classNames(
-        style.forecast_mainInfo,
-        'S--1-4',
-        'T--1-10',
-        'D--1-10'
-      )}
-    >
-      <div className={classNames(style.date_block, 'S--1-1')}>
+    <div className={classNames(style.forecast_mainInfo, 'S--1-4', 'T--1-12')}>
+      <div className={classNames(style.date_block, 'T--1-1')}>
         <p className={style.date_block_weekday}>{date.at(0)}</p>
         <p className={style.date_block_date}>{date.at(1)}</p>
         <p className={style.date_block_month}>{date.at(2)}</p>
       </div>
-      <div className={classNames(style.weather_details, 'S--2-4', 'T--2-10')}>
-        <table className={style.table}>
-          <thead className={style.table_head}>
-            <tr>
-              <td>&nbsp;</td>
-              <td className={style.table_elem} colSpan={2}>
-                Ніч
-              </td>
-              <td className={style.table_elem} colSpan={2}>
-                Ранок
-              </td>
-              <td className={style.table_elem} colSpan={2}>
-                День
-              </td>
-              <td className={style.table_elem} colSpan={2}>
-                Вечір
-              </td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>&nbsp;</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time ? style.selected_odd : style.odd
-                    )}
-                  >
-                    {hour.time.slice(hour.time.indexOf(' '))}
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
+      <div className={style.description}>
+        <div className={style.description_space}>&nbsp;</div>
+        <p>Температура</p>
+        <p>Відчувається</p>
+        <p>Вітер, м/с</p>
+        <p>Вологість, %</p>
+        <p>Тиск, мм</p>
+      </div>
+      <div className={classNames(style.table)}>
+        <Swiper
+          modules={[Pagination]}
+          slidesPerView={4}
+          breakpoints={{
+            400: { slidesPerView: 5 },
+            450: { slidesPerView: 6 },
+            550: { slidesPerView: 7 },
+            700: { slidesPerView: 8 },
+          }}
+          loop={false}
+        >
+          {weatherByHours.map((hour) => {
+            return (
+              <SwiperSlide
+                key={hour.time_epoch}
+                // className={style.table_element}
+              >
+                <p
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time ? style.selected_odd : style.odd
+                  )}
+                >
+                  {hour.time.slice(hour.time.indexOf(' '))}
+                </p>
+                <div
+                  className={classNames(
+                    style.table_item,
+                    markedTime === hour.time && style.selected_even
+                  )}
+                >
+                  <img
                     title={hour.condition.text}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time && style.selected_even
-                    )}
-                  >
-                    <img src={hour.condition.icon} alt={hour.condition.text} />
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td className={style.table_description}>Температура</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time ? style.selected_odd : style.odd
-                    )}
-                  >
-                    <p>{Math.round(hour.temp_c)} °C</p>
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td className={style.table_description}>Відчувається</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time && style.selected_even
-                    )}
-                  >
-                    <p>{Math.round(hour.feelslike_c)} °C</p>
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td className={style.table_description}>Вітер, м/с</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    title={'Вітер'}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time ? style.selected_odd : style.odd
-                    )}
-                  >
-                    <p>{((hour.wind_kph * 1000) / 3600).toFixed(2)}</p>
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td className={style.table_description}>Вологість, %</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    title={'Вологість'}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time && style.selected_even
-                    )}
-                  >
-                    <p>{hour.humidity}</p>
-                  </td>
-                );
-              })}
-            </tr>
-            <tr>
-              <td className={style.table_description}>Тиск, мм</td>
-              {weatherByHours.map((hour) => {
-                return (
-                  <td
-                    key={hour.time_epoch}
-                    title={'Тиск, мм рт.ст.'}
-                    className={classNames(
-                      style.table_elem,
-                      markedTime === hour.time ? style.selected_odd : style.odd
-                    )}
-                  >
-                    <p>{Math.trunc(hour.pressure_mb * 0.750063755419211)}</p>
-                  </td>
-                );
-              })}
-            </tr>
-          </tbody>
-        </table>
+                    className={style.table_item_elem}
+                    src={hour.condition.icon}
+                    alt={hour.condition.text}
+                  />
+                </div>
+                <p
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time ? style.selected_odd : style.odd
+                  )}
+                >
+                  {Math.round(hour.temp_c)} °C
+                </p>
+                <p
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time && style.selected_even
+                  )}
+                >
+                  {Math.round(hour.feelslike_c)} °C
+                </p>
+                <p
+                  title={'Вітер'}
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time ? style.selected_odd : style.odd
+                  )}
+                >
+                  {((hour.wind_kph * 1000) / 3600).toFixed(2)}
+                </p>
+                <p
+                  title={'Вологість'}
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time && style.selected_even
+                  )}
+                >
+                  {hour.humidity}
+                </p>
+                <p
+                  title={'Тиск, мм рт.ст.'}
+                  className={classNames(
+                    style.table_item_elem,
+                    markedTime === hour.time ? style.selected_odd : style.odd
+                  )}
+                >
+                  {Math.trunc(hour.pressure_mb * 0.750063755419211)}
+                </p>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
